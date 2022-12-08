bb2d713382d3afc1f635013527329b8ce1a14c0b
Ari Kim <107406265+lielocks@users.noreply.github.com>
2022-12-08 23:17:59 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 228 ms, Memory: 18400 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = 1;
+        int b = 1;
+        int c = 2;
+        int d = 2;
+        int e = 2;
+        int f = 8;
+
+        a = a - in.nextInt();
+        b = b - in.nextInt();
+        c = c - in.nextInt();
+        d = d - in.nextInt();
+        e = e - in.nextInt();
+        f = f - in.nextInt();
+
+        System.out.print(a + " ");
+        System.out.print(b + " ");
+        System.out.print(c + " ");
+        System.out.print(d + " ");
+        System.out.print(e + " ");
+        System.out.print(f + " ");
+        
+    }
+}
