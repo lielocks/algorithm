bb2a678bec57b6a314dc791775643278ece2ea37
Ari Kim <107406265+lielocks@users.noreply.github.com>
2022-12-10 16:50:32 +0900

    [Bronze V] Title: 나머지, Time: 220 ms, Memory: 17736 KB -BaekjoonHub

---
+++ 백준/Bronze/10430. 나머지/나머지.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        int a = in.nextInt();
+        int b = in.nextInt();
+        int c = in.nextInt();
+        
+        in.close();
+
+        System.out.println((a+b)%c);
+        System.out.println(((a%c)+(b%c))%c);
+        System.out.println((a*b)%c);
+        System.out.println(((a%c)*(b%c))%c);
+
+    }
+}
