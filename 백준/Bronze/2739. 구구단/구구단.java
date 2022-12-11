9edcc8c5ed2913f69c03168891867d1bbfc43a23
Ari Kim <107406265+lielocks@users.noreply.github.com>
2022-12-11 21:51:06 +0900

    [Bronze V] Title: 구구단, Time: 236 ms, Memory: 18584 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+        int a = in.nextInt();
+
+        for (int i = 1; i < 10; i++) {
+            System.out.println(a +" * " + i + " = " + i * a);
+        }
+
+    }
+}
