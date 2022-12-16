758998de3bdba36ff75c3418a7c399e4823e424e
Ari Kim <107406265+lielocks@users.noreply.github.com>
2022-12-16 14:45:19 +0900

    Create 최소 , 최대 v2.java

---
+++ 백준/Bronze/10818. 최소， 최대/최소 , 최대 v2.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+        int max = -1000001;
+        int min = 1000001;
+
+        while (st.hasMoreTokens()) {
+            int val = Integer.parseInt(st.nextToken());
+            if (val > max) {
+                max = val;
+            }
+            if (val < min) {
+                min = val;
+            }
+        }
+        System.out.println(min + " " + max);
+    }
+}
