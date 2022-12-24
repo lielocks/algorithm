c2d4f2cd5d7c437e1c09c0bcbc552127e74ba5d7
Ari Kim <107406265+lielocks@users.noreply.github.com>
2022-12-24 11:42:22 +0900

    Create 상수2.java

---
+++ 백준/Bronze/2908. 상수/상수2.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int a = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+        int b = Integer.parseInt(new StringBuilder(st.nextToken()).reverse().toString());
+
+        System.out.println(a > b ? a : b);
+        //if (a > b) System.out.println(a)
+        //if (!a > b) System.out.println(b)
+    }
+}
