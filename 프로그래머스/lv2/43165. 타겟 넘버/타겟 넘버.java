7e91ac8d28d13226b6ccdd0ccef74de7602d4099
Ari Kim <107406265+lielocks@users.noreply.github.com>
2023-02-21 18:34:44 +0900

    [level 2] Title: 타겟 넘버, Time: 0.36 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/lv2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,31 +1,6 @@
-import java.util.*;
-
 class Solution {
-    int answer;
-    int[] numbers;
-    int target;
-    
-    public void dfs(int index, int sum) {
-        //1. 탈출조건
-        if(numbers.length == index) {
-            if(sum == target) {
-                answer++;
-            }
-            return;
-        }
-        //2. 수행동작
-        dfs(index + 1, sum + numbers[index]);
-        dfs(index + 1, sum - numbers[index]);
-    }
-    
     public int solution(int[] numbers, int target) {
-        //마지막 노드까지 탐색했을때의 sum이 target과 일치하면 answer++
-        this.answer = answer;
-        this.target = target;
-        this.numbers = numbers;
-        
-        dfs(0, 0);
+        int answer = 0;
         return answer;
     }
-    
 }
