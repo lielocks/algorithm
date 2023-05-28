003587e4f428743e6c013c1a0a9a1a3115c88475
Ari Kim <107406265+lielocks@users.noreply.github.com>
2023-05-28 20:09:25 +0900

    [Bronze II] Title: 재귀의 귀재, Time: 340 ms, Memory: 20152 KB -BaekjoonHub

---
+++ 백준/Bronze/25501. 재귀의 귀재/재귀의 귀재.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int count;
+    static String sentence;
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int k = Integer.parseInt(br.readLine());
+        for(int i = 0; i < k; i++) {
+            sentence = br.readLine();
+            count = 0;
+            System.out.println(isPalindrome(0, sentence.length() - 1) + " " + count);
+        }
+
+    }
+
+    public static int isPalindrome(int r, int l) {
+        count++;
+        if (r >= l) {
+            return 1;
+        }else if(sentence.charAt(r) != sentence.charAt(l)) {
+            return 0;
+        }else {
+            return isPalindrome(r + 1, l - 1);
+        }
+    }
+}
