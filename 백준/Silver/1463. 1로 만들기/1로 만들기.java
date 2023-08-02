c372df2a7c30d9c4411b62d51463ee8cba916b40
Ari Kim <107406265+lielocks@users.noreply.github.com>
2023-08-02 21:40:33 +0900

    [Silver III] Title: 1로 만들기, Time: 256 ms, Memory: 70004 KB -BaekjoonHub

---
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.IOException;
+ 
+public class Main {
+ 
+	static Integer[] dp; // memoization 할 배열
+ 
+	public static void main(String[] args) throws IOException {
+ 
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+ 
+		int N = Integer.parseInt(br.readLine());
+ 
+		dp = new Integer[N + 1];
+		dp[0] = dp[1] = 0;
+ 
+		System.out.print(recur(N));
+ 
+	}
+ 
+	static int recur(int N) {
+ 
+		if (dp[N] == null) {
+			// 6으로 나눠지는 경우 
+			if (N % 6 == 0) {
+				dp[N] = Math.min(recur(N - 1), Math.min(recur(N / 3), recur(N / 2))) + 1;
+			}
+			// 3으로만 나눠지는 경우 
+			else if (N % 3 == 0) {
+				dp[N] = Math.min(recur(N / 3), recur(N - 1)) + 1;
+			}
+			// 2로만 나눠지는 경우 
+			else if (N % 2 == 0) {
+				dp[N] = Math.min(recur(N / 2), recur(N - 1)) + 1;
+			}
+			// 2와 3으로 나누어지지 않는 경우
+			else {
+				dp[N] = recur(N - 1) + 1;
+			}
+		}
+		return dp[N];
+	}
+}
+ 
