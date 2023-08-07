23156b65e4df6b6d3d02b784259ee1f36e874823
Ari Kim <107406265+lielocks@users.noreply.github.com>
2023-08-07 19:36:56 +0900

    Update 포도주 시식.java

--- DP/2156/포도주 시식.java
+++ DP/2156/포도주 시식.java
@@ -5,10 +5,10 @@ import java.io.IOException;
 public class Main {
     
     /**
-     * 1. i번째 포도주 잔을 마시지 않는 경우: dp[i] = dp[i - 1]
-     * 2. i번째 포도주 잔을 마시는 경우
-     * 2 - 1. 이전 포도주를 마시지 않은 경우(i - 1번째 포도주를 마시지 않음): dp[i] = dp[i - 2] + arr[i]
-     * 2 - 2. 이전 포도주를 마신 경우(i - 1번째 포도주를 마심): dp[i] = dp[i - 3] + arr[i - 1] + arr[i]
+     * 1 i번째 포도주 잔을 마시지 않는 경우: dp[i] = dp[i - 1]
+     * 2 i번째 포도주 잔을 마시는 경우
+     * 2 - 1 이전 포도주를 마시지 않은 경우(i - 1번째 포도주를 마시지 않음): dp[i] = dp[i - 2] + arr[i]
+     * 2 - 2 이전 포도주를 마신 경우(i - 1번째 포도주를 마심): dp[i] = dp[i - 3] + arr[i - 1] + arr[i]
      */
 	public static void main(String[] args) throws IOException {
  
