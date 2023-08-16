51d96031b0a8d1c095e16c9b1c473fd407c81d84
Ari Kim <107406265+lielocks@users.noreply.github.com>
2023-08-16 15:06:57 +0900

    [Silver II] Title: 쇠막대기, Time: 160 ms, Memory: 15704 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -1,37 +1,38 @@
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Stack;
 
 public class Main {
-    public static void main(String[] args) throws NumberFormatException, IOException {
-        BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
 
-        String input = bf.readLine();
-        Stack<Character> stack = new Stack<>();
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String input = br.readLine();
 
-        int result = 0;
+        int open = 0;
+        int answer = 0;
+
         for (int i = 0; i < input.length(); i++) {
+            char c = input.charAt(i);
+
             // 열린 괄호 ( 면 push
-            if (input.charAt(i) == '(') { 
-                stack.push('(');
+            if (c == '(') {
+                open++;
             }
             // 닫힌 괄호 ) 일 경우
-            if (input.charAt(i) == ')') { 
-                stack.pop();
+            else {
+                open--;
                 // 그 전 괄호가 열린 괄호 ) => 레이저
                 if (input.charAt(i - 1) == '(') {
-                    // 현재 stack 사이즈만큼 +
-                    result += stack.size(); 
+                    answer += open; 
                 }
                 // 그 전 괄호가 닫힌 괄호면 레이저 x 1만 +
                 else { 
-                    result++;
+                    answer++; 
                 }
             }
         }
 
-        System.out.println(result);
+        System.out.println(answer);
     }
 
 }
+
