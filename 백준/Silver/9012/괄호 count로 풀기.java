563a1faf998ce804cdb55045e00b8c29644c5895
Ari Kim <107406265+lielocks@users.noreply.github.com>
2023-08-16 13:22:37 +0900

    Create 괄호 count로 풀기.java

---
+++ 백준/Silver/9012/괄호 count로 풀기.java
@@ -0,0 +1,54 @@
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.BufferedReader;
+ 
+public class Main {
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+ 
+		int T = Integer.parseInt(br.readLine());
+ 
+		while (T-- > 0) {
+			sb.append(solve(br.readLine())).append('\n');
+		}
+		System.out.println(sb);
+ 
+	}
+ 
+	public static String solve(String s) {
+ 
+		int count = 0;
+ 
+		for (char c : s.toCharArray()) {
+ 
+			// 여는 괄호일 경우 카운트 증가
+			if (c == '(') {
+				count++;
+			}
+ 
+			// 아래는 모두 닫는 괄호 일 경우들이다.
+ 
+			// count 가 0인 경우. 즉, 닫는 괄호를 입력받았으나 pop할 원소가 없을 경우
+			else if (count == 0) {
+				return "NO";
+			}
+			// 그 외의 경우 count를 감소시킨다.
+			else {
+				count--;
+			}
+		}
+ 
+		/*
+		 * 모든 검사 마치고 잔여 요소가 있으면(count > 0) 여는 괄호가 많은 경우는 "NO" 
+		 * 요소가 비어있으면(count = 0) 온전한 수식이므로 "YES" 이다.
+		 */
+ 
+		if (count == 0) {
+			return "YES";
+		} 
+		else {
+			return "NO";
+		}
+	}
+}
