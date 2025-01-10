eaf4b5b6b21a4f0900acd1b6a4dc9a4ef31a19ad
Ari Kim <kari300084@gmail.com>
2025-01-10 10:45:34 +0900

    [level 3] Title: 정수 삼각형, Time: 9.11 ms, Memory: 64.3 MB -BaekjoonHub

---
+++ 프로그래머스/3/43105. 정수 삼각형/정수 삼각형.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[][] triangle) {
+        int n = triangle.length;
+        
+        for(int i = n - 2; i >= 0; i--) {
+            for(int j = 0; j < triangle[i].length; j++) {
+                triangle[i][j] = triangle[i][j] + Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
+            }
+        }
+        
+        return triangle[0][0];
+    }
+}
